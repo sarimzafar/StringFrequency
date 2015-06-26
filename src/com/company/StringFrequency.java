fae86b666c1007d1e556f3a78a3bcb92f5e92d76
Sarim Zafar <shzafar@uwaterloo.ca>
2015-06-26 12:11:32 -0400

    Updated the code to ensure efficiency of O(nlogn)

--- src/com/company/StringFrequency.java
+++ src/com/company/StringFrequency.java
@@ -1,31 +1,50 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Scanner;
-
-//Worst Case Complexity of O(n^2)
+import java.util.*;
 
 /**
  * Created by shassanzafar on 6/22/2015.
  */
 public class StringFrequency {
-    //Class variables to ensure ease of access
+
     LinkedHashMap<Character, Integer> hashMap = new LinkedHashMap<Character, Integer>();
     String word = "";
+    char [] chars = new char[]{};
+
+
+    public void sortWord()
+    {
+        chars = word.toCharArray();
+        Arrays.sort(chars); //nlog(n)
+    }
+
+    public int BinarySearch(int max,int min,char key)
+    {
+        if(max < min)
+            return 0;
+        else
+        {
+            int mid = (max+min)/2;
+            if(key == chars[mid])
+                return 1;
+            else if(key > chars[mid])
+                BinarySearch(max,mid+1,key);
+            else
+                BinarySearch(mid-1,min,key);
+        }
+        return 1;
+    }
 
 
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         StringFrequency stringFrequency = new StringFrequency();
-        System.out.println("Enter the word:");
         String temp = stringFrequency.word = obj.next();
-
+        stringFrequency.sortWord();
 
         for (int i = 0; i < temp.length(); i++) {
             stringFrequency.calcFrequency(temp.charAt(i));
         }
-        //for each entry in the Hashmap
-        for (HashMap.Entry<Character, Integer> entry : stringFrequency.hashMap.entrySet()) {
+
+        for (Map.Entry<Character, Integer> entry : stringFrequency.hashMap.entrySet()) {//for each entry in the entryset of the hashmap
             if (entry.getValue() != 1)
                 System.out.print(entry.getKey() + "" + entry.getValue());
             else
@@ -35,13 +54,15 @@ public class StringFrequency {
     }
 
     public void calcFrequency(Character character) {
-        int count = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (character == word.charAt(i))
-                count++;
-        }
-        
+        //do a binary search and update the linked hash map
+        int count = BinarySearch(chars.length-1,0,character);
         if(!hashMap.containsKey(character))
-            hashMap.put(character, count);
+            hashMap.put(character,count);
+        else
+        {
+            int prev = hashMap.get(character);
+            hashMap.replace(character,prev+count);
+        }
+
     }
 }
